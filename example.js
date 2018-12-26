24930a87460050ecee3013aa9b933c529a385d2b
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-26 18:31:08 +0100

    example

---
+++ example.js
@@ -0,0 +1,19 @@
+console.log('--- increment ---');
+
+  function plus_a(a) {
+    let result = ++a;
+    return [result, a];
+  }
+
+console.log('--- test cases ---')
+
+
+console.log('--- replication ---')
+
+  function plus_a(a) {
+    let result;
+    // 
+    return [result, a];
+  }
+
+// testing utils
